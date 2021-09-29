46eb0cbf8c664c88b730e4c27d141af7ff0fb9b2
Zenky <bzenky@gmail.com>
2021-09-29 07:38:57 -0300

    api post users + password crypto

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,4 +1,6 @@
 import dbConnect from '../../src/utils/dbConnect'
+import UsersModel from '../../src/models/users'
+import { crypto } from '../../src/utils/password'
 
 const users = async (req, res) => {
   const { method } = req
@@ -8,6 +10,33 @@ const users = async (req, res) => {
       await dbConnect()
       res.status(200).json({ sucess: true })
       break
+
+    case 'POST':
+      // pegar os dados no req
+      const {
+        name,
+        email,
+        password,
+      } = req.body
+
+      await dbConnect()
+
+      const passwordCrypto = await crypto(password)
+
+      const user = new UsersModel({
+        name,
+        email,
+        password: passwordCrypto,
+      })
+
+      user.save()
+
+      res.status(200).json({ success: true })
+
+    // conectar no banco
+    // criptografar senha
+    // salvar os dados
+    // responder sucesso
   }
 }
 
